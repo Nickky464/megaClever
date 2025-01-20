c66d423ab891cf9c2b0c3ea8f85c4921a81a9355
Chatchaya Thawon <nickkyt25@gmail.com>
2025-01-20 18:32:22 +0700

    add pump status

--- src/esp8266.cpp
+++ src/esp8266.cpp
@@ -6,86 +6,120 @@
 #include <WiFiUdp.h>
 
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "TMPL6gdQcodak"                   // Blynk template id
+#define BLYNK_TEMPLATE_ID "TMPL6gdQcodak"                   // Blynk template ID
 #define BLYNK_TEMPLATE_NAME "ppl2"                          // Blynk template name
 #define BLYNK_AUTH_TOKEN "AwRQJZLjIt88wFDR2WtWIV2jFMRSqYv2" // Blynk auth token
 
-#include <BlynkSimpleEsp8266.h> // Board-specific header for ESP8266 Blynk
+#include <BlynkSimpleEsp8266.h> // Blynk header for ESP8266
 
-// WiFi Credentials
-// Set password to "" for open networks.
-char ssid[] = "P-E-A-R";  // WiFi SSID "e.g. WIFINAME"
-char pass[] = "pnpnpnpn"; // WiFi PASSWORD "e.g. 12345678"
+// WiFi credentials
+char ssid[] = "P-E-A-R";
+char pass[] = "pnpnpnpn";
 
+// Variables for sensor data
 float get_temperature = 0.0f;
 float get_humidity = 0.0f;
 float get_co2 = 0.0f;
+boolean get_pump_status = false;
 int get_tempLimit = -1;
 boolean tempState = false;
 
+// Pin definitions
 const int RELAY_PIN = D2;
 
-// Setup SoftwareSerial
+// SoftwareSerial for MEGA2560 communication
 SoftwareSerial MEGA2560_Serial(D2, D3); // (RX, TX)
 
-// NTP Client Setup
+// NTP client setup
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 7 * 3600, 60000); // UTC+7 timezone
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 7 * 3600, 60000);
 
-// Web Server Setup
+// Web server setup
 ESP8266WebServer server(80);
 
+// Function prototypes
 float extractValue(String data, String startDelimiter, String endDelimiter);
+void sendToBlynk();
+void resetMCU();
+void getParsedData();
+void handleRoot();
+void handleNotFound();
+void checkTimeForRollMotor();
 
+// Setup function
+void setup()
+{
+  Serial.begin(9600);
+  MEGA2560_Serial.begin(9600);
+
+  WiFi.begin(ssid, pass);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Connecting to WiFi...");
+    delay(1000);
+  }
+  Serial.println("WiFi connected!");
+
+  timeClient.begin();
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+  server.begin();
+
+  Serial.println("Web server started");
+  pinMode(RELAY_PIN, OUTPUT);
+}
+
+// Main loop
+void loop()
+{
+  Blynk.run();
+  timeClient.update();
+  server.handleClient();
+
+  if (MEGA2560_Serial.available())
+  {
+    getParsedData();
+    sendToBlynk();
+    checkTimeForRollMotor();
+    delay(15000);
+  }
+}
+
+// Blynk write handler
 BLYNK_WRITE(InternalPinDBG)
 {
   if (String(param.asStr()) == "reboot")
   {
     Serial.println("Rebooting...");
-
-    // TODO: Perform any neccessary preparation here,
-    // i.e. turn off peripherals, write state to EEPROM, etc.
-
-    // NOTE: You may need to defer a reboot,
-    // if device is in process of some critical operation.
-
     resetMCU();
   }
 }
 
-// Function to extract a value between two delimiters
+// Extract a value from a delimited string
 float extractValue(String data, String startDelimiter, String endDelimiter)
 {
   int startIdx = data.indexOf(startDelimiter) + startDelimiter.length();
   int endIdx = data.indexOf(endDelimiter, startIdx);
-
-  String valueStr = data.substring(startIdx, endIdx); // Extract the value as a string
-  return valueStr.toFloat();                          // Convert the string to a float and return
+  return data.substring(startIdx, endIdx).toFloat();
 }
 
+// Send data to Blynk
 void sendToBlynk()
 {
   static unsigned long lastSendTime = 0;
   if (millis() - lastSendTime >= 1000)
   {
-    Serial.print("sending data to blynk cloud");
     lastSendTime = millis();
-
-    // if (get_temperature >= 100) get_temperature = 0;
-    // if (get_humidity >= 70) get_humidity = 0;
-    // if (get_co2 >= 70) get_co2 = 0;
-    Serial.println(get_temperature + get_humidity + get_co2);
-
     Blynk.virtualWrite(V0, get_temperature);
     Blynk.virtualWrite(V1, get_humidity);
     Blynk.virtualWrite(V2, get_co2);
-    if (get_tempLimit != -1 && get_temperature > get_tempLimit)
+
+    if (get_tempLimit != -1 && get_temperature > get_tempLimit && !tempState)
     {
-      if (!tempState)
-      {
-        Serial.println("Over Temperature Alert!");
-        tempState = true;
-      }
+      Serial.println("Over Temperature Alert!");
+      tempState = true;
     }
     else
     {
@@ -94,53 +128,49 @@ void sendToBlynk()
   }
 }
 
+// Reset the MCU
 void resetMCU()
 {
-#if defined(ARDUINO_ARCH_MEGAAVR)
-  wdt_enable(WDT_PERIOD_8CLK_gc);
-#elif defined(__AVR__)
-  wdt_enable(WDTO_15MS);
-#elif defined(__arm__)
-  NVIC_SystemReset();
-#elif defined(ESP8266) || defined(ESP32)
+#if defined(ESP8266) || defined(ESP32)
   ESP.restart();
 #else
-#error "MCU reset procedure not implemented"
-#endif
   for (;;)
   {
   }
+#endif
 }
 
+// Parse incoming data from MEGA2560
 void getParsedData()
 {
-  String receivedData = MEGA2560_Serial.readString(); // Read the incoming string
-  // String receivedData = "Temperature: 23.00 °C, Humidity: 40.00 %, CO2: 59.73 %"; // Read the incoming string
-
-  // Print the received data
+  String receivedData = MEGA2560_Serial.readString();
   Serial.print("Received data: ");
+  // Temperature: 23.5 °C, Humidity: 60 %, CO2: 26 %, Pump: True
   Serial.println(receivedData);
 
-  // Now parse the received string to extract temperature, humidity, and CO2 values
-
-  // Example: "Temperature: 25.0 °C, Humidity: 60.0 %, CO2: 450 PPM"
-
-  // Extract temperature
   get_temperature = extractValue(receivedData, "Temperature: ", " °C");
-  // Extract humidity
   get_humidity = extractValue(receivedData, "Humidity: ", " %");
-  // Extract CO2 concentration
   get_co2 = extractValue(receivedData, "CO2: ", " %");
 
-  // Print the parsed values
-  Serial.print("Parsed Temperature: ");
-  Serial.println(get_temperature); // Temperature as float
-  Serial.print("Parsed Humidity: ");
-  Serial.println(get_humidity); // Humidity as float
-  Serial.print("Parsed CO2: ");
-  Serial.println(get_co2); // CO2 concentration as int
+  String pumpStatusStr = receivedData.substring(receivedData.indexOf("Pump: ") + 6); // Get the part after "Pump: "
+  if (pumpStatusStr == "True")
+  {
+    get_pump_status = true; // Pump is ON
+  }
+  else if (pumpStatusStr == "False")
+  {
+    get_pump_status = false; // Pump is OFF
+  }
+
+  Serial.print("Temperature: ");
+  Serial.println(get_temperature);
+  Serial.print("Humidity: ");
+  Serial.println(get_humidity);
+  Serial.print("CO2: ");
+  Serial.println(get_co2);
 }
 
+// Handle root web page
 void handleRoot()
 {
   String html = "<html><body>";
@@ -152,70 +182,26 @@ void handleRoot()
   server.send(200, "text/html", html);
 }
 
+// Handle 404 errors
 void handleNotFound()
 {
   server.send(404, "text/plain", "404: Not Found");
 }
 
+// Check time and control relay
 void checkTimeForRollMotor()
 {
   int currentHour = timeClient.getHours();
   int currentMinute = timeClient.getMinutes();
 
-  if ((currentHour == 6) || (currentHour == 7) || (currentHour == 8 && currentMinute == 0))
+  if ((currentHour >= 6 && currentHour <= 8) && (currentHour != 8 || currentMinute == 0))
   {
-    digitalWrite(RELAY_PIN, HIGH); // Turn relay on
-    Serial.println("Relay turned ON between 6:00 and 8:00 UTC+7");
+    digitalWrite(RELAY_PIN, HIGH);
+    Serial.println("Relay ON: 6:00 - 8:00 UTC+7");
   }
   else
   {
-    digitalWrite(RELAY_PIN, LOW); // Turn relay off
-    Serial.println("Relay turned OFF outside 6:00 to 8:00 UTC+7");
-  }
-}
-
-void setup()
-{
-  Serial.begin(9600); // Initialize Serial Monitor for debugging
-
-  // Start MEGA2560_Serial communication with the ATmega2560
-  MEGA2560_Serial.begin(9600);
-
-  WiFi.begin(ssid, pass);
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("Connecting to WiFi...");
-    delay(1000);
-  }
-  Serial.println("Connected to WiFi!");
-
-  timeClient.begin();
-
-  server.on("/", handleRoot);
-  server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println("Web server started");
-
-  Serial.println("ESP8266 is ready to receive data from ATmega2560.");
-}
-
-void loop()
-{
-  Blynk.run();
-
-  timeClient.update();
-
-  server.handleClient();
-
-  if (MEGA2560_Serial.available())
-  {
-    getParsedData();
-
-    sendToBlynk();
-
-    checkTimeForRollMotor();
-
-    delay(15000);
+    digitalWrite(RELAY_PIN, LOW);
+    Serial.println("Relay OFF");
   }
 }
--- src/mega2560.cpp
+++ src/mega2560.cpp
@@ -2,126 +2,64 @@
 #include <DHT.h>
 #include <SoftwareSerial.h>
 
-// Define which analog input channel you are going to use
-#define MG_PIN (A3)
-#define BOOL_PIN (2)
-#define DC_GAIN (8.5) // Define the DC gain of amplifier
+// Pin Definitions
+#define MG_PIN A3 // Analog pin for CO2 sensor
+#define BOOL_PIN 2 // Pin for additional sensor (unused here)
+#define RELAY_PIN 4 // Relay control pin
+#define DHT_PIN 2 // DHT sensor pin
 
-/***********************Software Related Macros************************************/
-#define READ_SAMPLE_INTERVAL (50) // Sample interval in milliseconds
-#define READ_SAMPLE_TIMES (5)     // Number of samples to average
+// Constants
+#define DC_GAIN 8.5 // Amplifier gain
+#define READ_SAMPLE_INTERVAL 50 // Sampling interval in ms
+#define READ_SAMPLE_TIMES 5 // Number of samples to average
+#define ZERO_POINT_VOLTAGE 0.220 // Voltage at 400 PPM CO2
+#define REACTION_VOLTAGE 0.020 // Voltage drop at 1000 PPM CO2
+#define DHT_TYPE DHT11 // DHT sensor type
 
-/**********************Application Related Macros**********************************/
-// These two values differ from sensor to sensor. User should determine this value.
-#define ZERO_POINT_VOLTAGE (0.220) // Voltage at 400 PPM CO2
-#define REACTION_VOLTAGE (0.020)   // Voltage drop at 1000 PPM CO2
+// CO2 Calibration
+float CO2Curve[3] = {2.602, ZERO_POINT_VOLTAGE, REACTION_VOLTAGE / (2.602 - 3)};
+float slope = 2000.0;
+float offset = 0.0;
 
-// CO2 Read
-float slope(2000.0);
-float offset(0.0);
+// Variables
+float temp_c = 0.0; // Temperature in Celsius
+float humidity = 0.0; // Humidity percentage
+boolean pump_status = false; // Pump status
 
-// DHT
-#define DHT_PIN (2)
-#define DHT_TYPE (DHT11)
+// Objects
+DHT dht(DHT_PIN, DHT_TYPE); // Initialize DHT sensor
+SoftwareSerial ESP8266_Serial(6, 7); // ESP8266 communication (RX, TX)
 
-DHT dht(DHT_PIN, DHT_TYPE); // Initialize the DHT sensor
+// Function Prototypes
+float MGReadCO2(int MG_PIN, float slope, float offset);
+float MGRead(int MG_PIN);
+int MGGetPercentage(float volts, float *pcurve);
+void sendToESP8266(float temp_c, float humidity, int percentage, boolean pump_status);
 
-// ESP8266 Serial comunicate with SoftwareSerial
-SoftwareSerial ESP8266_Serial(6, 7);
+// Setup function
+void setup() {
+  Serial.begin(9600); // Debug communication
+  ESP8266_Serial.begin(9600); // ESP8266 communication
+  dht.begin(); // Initialize DHT sensor
 
-float CO2Curve[3] = {2.602, ZERO_POINT_VOLTAGE, (REACTION_VOLTAGE / (2.602 - 3))};
-
-// Declare variables
-float temp_c;
-float humidity;
-int RELAY_PIN = 4; // Relay pin
-boolean tempState = false;
-boolean pump_status = false;
-
-float get_temperature = 0.0f;
-float get_humidity = 0.0f;
-int get_tempLimit = -1;
-
-// Declare function prototypes
-// float MGReadCO2(int MG_PIN, float slope, float offset); // Function to read CO2 concentration in PPM
-// float MGRead(int MG_PIN);                               // Function to read raw voltage
-// int MGGetPercentage(float volts, float *pcurve);        // Function to calculate CO2 percentage
-
-// Define the function to read CO2 concentration
-float MGReadCO2(int MG_PIN, float slope, float offset)
-{
-  int sensorValue = analogRead(MG_PIN);         // Read the raw sensor value
-  float voltage = sensorValue * (5.0 / 1023.0); // Convert to voltage (assuming 5V reference)
-
-  // Convert voltage to ppm
-  float ppm = (slope * voltage) + offset;
-
-  // Convert ppm to percentage
-  // float percentage = ppm / 10000.0;
-
-  // Return both values as a struct
-  // data = {ppm, percentage};
-  return ppm;
-}
-
-// Read voltage from the analog sensor pin
-float MGRead(int mg_pin)
-{
-  return analogRead(mg_pin) * (3.3 / 1023.0); // For 3.3V ESP32
-}
-
-// Calculate CO2 concentration percentage based on the voltage and calibration curve
-int MGGetPercentage(float volts, float *pcurve)
-{
-  if (volts < pcurve[1])
-  {
-    return -1; // Below valid range
-  }
-  return int((volts - pcurve[1]) / (pcurve[2] - pcurve[1]) * 100);
-}
-
-// Send Temperature, Humidity, and CO2 data to the ESP8266
-void sendToESP8266(float temp_c, float humidity, int percentage, boolean pump_status)
-{
-  // Create a formatted message to send
-  String message = "Temperature: " + String(temp_c) + " °C, Humidity: " + String(humidity) + " %, CO2: " + String(percentage) + " %" + "Pump";
-
-  // Send the message over ESP8266_Serial (to ESP8266)
-  Serial.println(message);
-  ESP8266_Serial.println(message);
-}
-
-void setup()
-{
-  // Start Serial communication with the computer (for debugging)
-  Serial.begin(9600);
-
-  // Start ESP8266_Serial communication with the ESP8266
-  ESP8266_Serial.begin(9600); // Set the baud rate to communicate with ESP8266
-
-  dht.begin();
+  pinMode(RELAY_PIN, OUTPUT); // Set relay pin as output
 
   Serial.println("Starting communication with ESP8266...");
 }
 
-void loop()
-{
-  // Serial.print("ESP8266 Board MAC Address:  ");
-
+// Main loop
+void loop() {
   // Read CO2 sensor
   float co2value = MGReadCO2(MG_PIN, slope, offset);
   float percentage = (co2value / 10000) * 100;
 
-  Serial.print("CO2: ");
-  if (percentage == -1)
-  {
-    Serial.println("<400 PPM");
-  }
-  else
-  {
+  if (percentage == -1) {
+    Serial.println("CO2: <400 PPM");
+  } else {
+    Serial.print("CO2: ");
     Serial.print(co2value);
     Serial.print(" PPM");
-    Serial.print("CO2: ");
+    Serial.print(" >> ");
     Serial.print(percentage);
     Serial.println(" %");
   }
@@ -130,8 +68,7 @@ void loop()
   temp_c = dht.readTemperature();
   humidity = dht.readHumidity();
 
-  if (!isnan(temp_c) && !isnan(humidity))
-  {
+  if (!isnan(temp_c) && !isnan(humidity)) {
     Serial.print("Temperature: ");
     Serial.print(temp_c);
     Serial.print(" °C >> Humidity: ");
@@ -139,19 +76,43 @@ void loop()
     Serial.println(" %");
 
     // Control relay based on humidity
-    if (humidity <= 70)
-    {
+    if (humidity <= 70) {
       digitalWrite(RELAY_PIN, HIGH);
       pump_status = true;
+    } else {
+      digitalWrite(RELAY_PIN, LOW);
+      pump_status = false;
     }
-
-    // Send CO2, Temperature, and Humidity to ESP8266
-    sendToESP8266(temp_c, humidity, percentage, pump_status);
-  }
-  else
-  {
+  } else {
     Serial.println("Failed to read from DHT sensor!");
   }
 
-  delay(10000);
+  // Send data to ESP8266
+  sendToESP8266(temp_c, humidity, percentage, pump_status);
+
+  delay(10000); // Wait for 10 seconds
+}
+
+// Function Definitions
+float MGReadCO2(int MG_PIN, float slope, float offset) {
+  int sensorValue = analogRead(MG_PIN);
+  float voltage = sensorValue * (5.0 / 1023.0); // Convert to voltage
+  return (slope * voltage) + offset; // Calculate CO2 in PPM
+}
+
+float MGRead(int MG_PIN) {
+  return analogRead(MG_PIN) * (3.3 / 1023.0); // Convert to 3.3V voltage
+}
+
+int MGGetPercentage(float volts, float *pcurve) {
+  if (volts < pcurve[1]) {
+    return -1; // Below valid range
+  }
+  return int((volts - pcurve[1]) / (pcurve[2] - pcurve[1]) * 100);
+}
+
+void sendToESP8266(float temp_c, float humidity, int percentage, boolean pump_status) {
+  String message = "Temperature: " + String(temp_c) + " °C, Humidity: " + String(humidity) + " %, CO2: " + String(percentage) + " %, Pump: " + String(pump_status);
+  Serial.println(message);
+  ESP8266_Serial.println(message);
 }
